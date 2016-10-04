2b744122ea0da24117763cae118b4b3ba4d0c7c8
sooraj <srjshingari@gmail.com>
2016-10-04 18:57:03 +0530

    sooraj

---
+++ app/src/main/java/soorajshingari/kietmessenger/MyFirebaseMessagingService.java
@@ -0,0 +1,36 @@
+package soorajshingari.kietmessenger;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by sooraj on 02-10-2016.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Intent intent=new Intent(MyFirebaseMessagingService.this,Post.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent resultIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        Uri notificationSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(this);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("College Quora")
+                .setContentTitle(remoteMessage.getNotification().getBody())
+                .setAutoCancel(true)
+                .setSound(notificationSoundUri)
+                .setContentIntent(resultIntent);
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+
+    }
+}
